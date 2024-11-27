69fb5448cf7005b96ac72b24f7d1ca330a3c6569
Mane <61853435+Mane-jaker@users.noreply.github.com>
2024-11-26 18:05:41 -0600

    PREDICTION

--- app/listener/heatmap_listener.py
+++ app/listener/heatmap_listener.py
@@ -4,15 +4,27 @@ import threading
 from app.services.heatmap_service import HeatmapService
 
 heatmap_service = HeatmapService()
+random_data_generated = False  # Bandera para controlar la ejecución de generate_random_data
 
-def job_generate_heatmap():
-    heatmap_service.generate_heatmap()
+def job_generate_heatmap_and_predict():
+    global random_data_generated
+    timestamp = int(time.time())
+    heatmap_service.generate_heatmap(timestamp)
+    heatmap_service.predict_user_density(timestamp)
+    
+    # Ejecutar generate_random_data una vez después de predict_user_density
+    if not random_data_generated:
+        heatmap_service.generate_random_data(timestamp)
+        random_data_generated = True
 
 def run_scheduler():
-    schedule.every(1).minutes.do(job_generate_heatmap)
+    schedule.every(1).minutes.do(job_generate_heatmap_and_predict)
     while True:
         schedule.run_pending()
-        time.sleep(1)
+        # Calcular el tiempo restante hasta el próximo minuto
+        now = time.time()
+        sleep_time = 60 - (now % 60)
+        time.sleep(sleep_time)
 
 # Iniciar el hilo del scheduler
 scheduler_thread = threading.Thread(target=run_scheduler)
--- app/services/heatmap_service.py
+++ app/services/heatmap_service.py
@@ -1,15 +1,16 @@
 from prophet import Prophet
+import pandas as pd
 from app.db import get_firestore_db, get_realtime_db
 from geopy.distance import geodesic
-import time
-import pandas as pd
+import random
+import math
 
 class HeatmapService:
     def __init__(self):
         self.db_firestore = get_firestore_db()
         self.db_realtime = get_realtime_db()
         
-    def generate_heatmap(self):
+    def generate_heatmap(self, timestamp):
         firestore_db = self.db_firestore
         activities_ref = firestore_db.collection('user_activities')
         activities = activities_ref.where('is_active', '==', True).stream()
@@ -41,7 +42,6 @@ class HeatmapService:
         
         # Guardar en Realtime Database
         realtime_db = self.db_realtime
-        timestamp = int(time.time())
         heatmap_ref = realtime_db.child('heatmap').child(str(timestamp))
         
         # Imprimir los datos que se van a enviar a Firebase
@@ -49,32 +49,48 @@ class HeatmapService:
         
         heatmap_ref.set(heatmap_data)
         
-        
-    def predict_user_density(self):
+    def predict_user_density(self, timestamp):
+        print("Iniciando predicción de densidad de usuarios...")
         db = self.db_realtime
         heatmap_ref = db.child('heatmap')
-        end_time = int(time.time())
+        end_time = timestamp
         start_time = end_time - (60 * 60)  # Últimos 60 minutos
         
-        # Obtener todas las ubicaciones únicas
-        unique_locations = set()
-        for timestamp in range(start_time, end_time + 1):
-            data = heatmap_ref.child(str(timestamp)).get()
+        print(f"Tiempo de inicio: {start_time}, tiempo de fin: {end_time}")
+        
+        # Obtener todas las ubicaciones únicas y combinar las cercanas
+        unique_locations = {}
+        for ts in range(start_time, end_time + 1, 60):  # Incrementar de 60 en 60 segundos (1 minuto)
+            print(f"Obteniendo datos para timestamp {ts}...")
+            data = heatmap_ref.child(str(ts)).get()
             if data:
-                for location in data.keys():
-                    unique_locations.add(location)
+                print(f"Datos obtenidos para timestamp {ts}: {data}")
+                for location, density in data.items():
+                    found = False
+                    for unique_location in unique_locations.keys():
+                        if geodesic(unique_location.replace('_', '.').split(','), location.replace('_', '.').split(',')).kilometers <= 0.1:
+                            unique_locations[unique_location] += density
+                            found = True
+                            break
+                    if not found:
+                        unique_locations[location] = density
+        
+        print(f"Ubicaciones únicas obtenidas: {unique_locations}")
+        
         
         # Predecir la densidad de usuarios para cada ubicación
         predictions = {}
         for location in unique_locations:
             heatmap_data = []
-            for timestamp in range(start_time, end_time + 1):
-                data = heatmap_ref.child(str(timestamp)).get()
+            for ts in range(start_time, end_time + 1, 60):  # Incrementar de 60 en 60 segundos (1 minuto)
+                data = heatmap_ref.child(str(ts)).get()
                 if data and location in data:
                     density = data[location]
-                    heatmap_data.append({'timestamp': timestamp, 'density': density})
+                    heatmap_data.append({'timestamp': ts, 'density': density})
             
-            if heatmap_data:
+            print(f"Datos de heatmap para la ubicación {location}: {heatmap_data}")
+            
+            if len(heatmap_data) >= 2:  # Asegurarse de que hay al menos dos puntos de datos
                 df = pd.DataFrame(heatmap_data)
                 df['ds'] = pd.to_datetime(df['timestamp'], unit='s')
                 df['y'] = df['density']
@@ -88,7 +104,46 @@ class HeatmapService:
                 # Obtener el último valor de yhat
                 last_prediction = forecast.iloc[-1]
                 predictions[location] = last_prediction['yhat']
+            else:
+                print(f"No hay suficientes datos para la ubicación {location} para hacer una predicción.")
         
         # Guardar las predicciones en Realtime Database
         predict_heatmap_ref = db.child('predict_heatmap').child(str(end_time))
+        
+        # Imprimir las predicciones que se van a enviar a Firebase
+        print("Predicciones que se envían a Firebase:", predictions)
+        
         predict_heatmap_ref.set(predictions)
+    
+    def generate_random_data(self, timestamp):
+        print("Generando datos aleatorios...")
+        db = self.db_realtime
+        end_time = timestamp
+        reference_point = (16.623413, -93.100081)
+        lat_range = 0.6 / 111  # 1 km in degrees of latitude
+        lon_range = 0.6 / (111 * math.cos(math.radians(reference_point[0])))  # 1 km in degrees of longitude
+        
+        # Definir 5 ubicaciones fijas dentro de 1 km del punto de referencia
+        fixed_locations = [
+            (reference_point[0] + lat_range / 2, reference_point[1] + lon_range / 2),
+            (reference_point[0] - lat_range / 2, reference_point[1] - lon_range / 2),
+            (reference_point[0] + lat_range / 3, reference_point[1] - lon_range / 3),
+            (reference_point[0] - lat_range / 3, reference_point[1] + lon_range / 3),
+            (reference_point[0], reference_point[1] + lon_range / 4)
+        ]
+        
+        for i in range(30):  # Generar datos para los últimos 30 minutos
+            ts = end_time - (i * 60)
+            heatmap_data = {}
+            for location in fixed_locations:
+                latitude, longitude = location
+                location_str = f"{str(latitude).replace('.', '_')},{str(longitude).replace('.', '_')}"
+                density = random.randint(1, 10)
+                heatmap_data[location_str] = density
+            
+            # Imprimir los datos generados para depuración
+            print(f"Datos generados para timestamp {ts}: {heatmap_data}")
+            
+            # Guardar en Realtime Database
+            heatmap_ref = db.child('heatmap').child(str(ts))
+            heatmap_ref.set(heatmap_data)
