abe4901b15fa20bcf4a80ac441d0207481303da7
Mane <61853435+Mane-jaker@users.noreply.github.com>
2024-12-05 22:49:48 -0600

    app

--- app/repositories/vehicle_repository.py
+++ app/repositories/vehicle_repository.py
@@ -6,7 +6,6 @@ class VehicleRepository:
     def __init__(self, db: any):
         self.db = db
         
-    @staticmethod
     def create_vehicle(self, vehicle: Vehicle):
-        vehicle_ref = self.db.collection('users').document(vehicle.id)
+        vehicle_ref = self.db.collection('users').document()
         vehicle_ref.set(vehicle.to_dict())
